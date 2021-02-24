06fb05da8a72a4cabe2726b973a918d8605d7b47
Kaj Siebert <kaj@k-si.com>
2021-02-24 16:42:10 +0100

    Add skillsfinder view

--- skillsfinder/views.py
+++ skillsfinder/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    context = {
+        'site_name': "Skillsfinder",
+    }
+    return render(request, 'skillsfinder/index.html', context)
